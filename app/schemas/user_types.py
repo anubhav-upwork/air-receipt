5b4e3290c8fb50fe2c6eb6606bdd4261779d706b
anubhav <rohatgianubhav@gmail.com>
2023-07-29 22:29:40 +0530

    modifying schema user_types.py -- InDB method

--- app/schemas/user_types.py
+++ app/schemas/user_types.py
@@ -1,21 +1,49 @@
-from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+
+class UserType_Base(BaseModel):
+    user_type: str
+    usage_limit_days: int = 1
+    created_at: datetime
 
 
 # Schema for User Type Creation
-class User_TypeCreate(BaseModel):
+class User_TypeCreate(UserType_Base):
     user_type: str
     usage_limit_days: int
+    created_at = datetime.now()
 
 
 # Schema for User Type Retrieval
-class User_TypeResponse(User_TypeCreate):
+class User_Type(UserType_Base):
     id: int
     user_type: str
     usage_limit_days: int
 
     class Config:
-        from_attributes = True
+        orm_mode = True
 
 
-class User_TypeUpdate(User_TypeCreate):
+class User_TypeUpdate(UserType_Base):
+    id: Optional[int] = None
+    user_type: str
     usage_limit_days: int
+
+
+class User_TypeInDBBase(UserType_Base):
+    id: Optional[int] =  None
+    user_type: str
+
+    class Config:
+        orm_mode = True
+
+
+class User_Type(User_TypeInDBBase):
+    pass
+
+class User_TypeInDB(User_TypeInDBBase):
+    user_type: str
+
+
