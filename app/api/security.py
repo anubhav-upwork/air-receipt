e40f4c9208cec56318f78744e44d1ddff67d5457
anubhav <rohatgianubhav@gmail.com>
2023-08-01 23:05:33 +0530

    password hasher security.py and user_info_route.py

---
+++ app/api/security.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+from typing import Any, Union
+from jose import jwt
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+def hash_password(password: str) -> str:
+    """Create hashed password."""
+    return generate_password_hash(
+        password,
+        method='sha256'
+    )
+
+
+def validate_password(password: str, pwhash: str) -> bool:
+    """Check hashed password."""
+    return check_password_hash(pwhash, password)
