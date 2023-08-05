1e4f82aeed67bf6b5d4c3d91aa4f8b95e19ea6fc
anubhav <rohatgianubhav@gmail.com>
2023-08-05 09:35:17 +0530

    added document schemas - class and category

---
+++ app/schemas/document/document_category.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+from app.schemas.schema_utils import to_camel
+
+
+# Schema of User Type Base Class
+class DocumentCategory_Base(BaseModel):
+    category: str
+    category_code: int
+
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
+
+
+# Schema for Update of User_Type
+class DocumentCategory_Update(DocumentCategory_Base):
+    category: Optional[str]
+    category_code: Optional[int]
+
+
+# Schema for User Type Creation
+class DocumentCategory_Create(DocumentCategory_Base):
+    pass
+
+
+# Schema for User Type Retrieval
+class DocumentCategory(DocumentCategory_Base):
+    id: int
+    user_type: str
+    usage_limit_days: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
---
+++ app/schemas/document/document_class.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+from app.schemas.schema_utils import to_camel
+
+
+# Schema of User Type Base Class
+class DocumentClass_Base(BaseModel):
+    doc_class: str
+    doc_class_code: int
+
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
+
+
+# Schema for Update of User_Type
+class DocumentClass_Update(DocumentClass_Base):
+    doc_class: Optional[str]
+    doc_class_code: Optional[int]
+
+
+# Schema for User Type Creation
+class DocumentClass_Create(DocumentClass_Base):
+    pass
+
+
+# Schema for User Type Retrieval
+class DocumentClass(DocumentClass_Base):
+    id: int
+    doc_class: str
+    doc_class_code: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
