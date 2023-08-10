e46c4596ed36fedd61c68a59ccb1313f1b5d6dca
anubhav <rohatgianubhav@gmail.com>
2023-08-10 14:16:01 +0530

    data to populate in DB

---
+++ app/db_data/user_roles_data.py
@@ -0,0 +1,14 @@
+USER_ROLES_DATA = [
+    {
+        "user_role": "superadmin1",
+        "user_access_level": 2
+    },
+    {
+        "user_role": "admin1",
+        "user_access_level": 1
+    },
+    {
+        "user_role": "tenant1",
+        "user_access_level": 0
+    }
+]
---
+++ app/db_data/user_types_data.py
@@ -0,0 +1,14 @@
+USER_TYPES_DATA = [
+    {
+        "user_type": "premium",
+        "usage_limit_days": 365
+    },
+    {
+        "user_type": "monthly",
+        "usage_limit_days": 30
+    },
+    {
+        "user_type": "trail",
+        "usage_limit_days": 1
+    }
+]
