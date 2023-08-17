eeb7c3f07725d3f6a50709f8aecde6360b790ad9
anubhav <rohatgianubhav@gmail.com>
2023-08-17 23:11:28 +0530

    Update document_category_route.py, document_class_route.py, and document_user_route.py

--- app/api/api_v1/endpoints/document/document_user_route.py
+++ app/api/api_v1/endpoints/document/document_user_route.py
@@ -1,40 +1,67 @@
+import logging
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException, File, UploadFile, Form
 from sqlalchemy.orm import Session
+from werkzeug.security import generate_password_hash
+
+from app.api import deps
 from app.models.user.user_info import User_Info
 from app.models.documents.document_user import Document_User, DocumentSrc, DocumentType, DocumentState, DocumentReview
-from app.schemas.document.document_user import DocumentUser, DocumentUser_Create, DocumentUser_Update, DocumentUser_Upload
+from app.schemas.document.document_user import DocumentUser, DocumentUser_Create, DocumentUser_Update, \
+    DocumentUser_Upload
 from app.crud.document.document_user import get_document_user_service
 from app.crud.document.document_category import get_document_category_service
-from app.crud.user.user_info import get_user_info_service
-from werkzeug.security import generate_password_hash
-from app.api import deps
+from app.crud.document.document_class import get_document_class_service
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/documents", tags=["Document"])
 
 
 @router.post("/upload_document", status_code=201, response_model=DocumentUser)
-async def upload_document(du: DocumentUser_Upload,
-                          # file: UploadFile = File(...),
+async def upload_document(du: DocumentUser_Upload = Depends(),
+                          file: UploadFile = File(...),
                           db: Session = Depends(deps.get_db),
                           cur_user: User_Info = Depends(deps.get_current_user)) -> Document_User:
-    dc = du["params"]
-    file_name_hash = generate_password_hash(dc.document_id, method='md5')
+    logger.info(f"Document Received: from:{cur_user.id}, file:{file.filename},"
+                f" State:{du.document_state},"
+                f" ClassId:{du.document_class}")
+
+    file_name_hash = generate_password_hash(file.filename, method='md5')
     existing_filehash = get_document_user_service.get_by_doc_id(db_session=db, doc_id=file_name_hash)
-    # existing_class_code = get_document_class_service.get_by_docclasscode(db_session=db,
-    #                                                                      doc_class_code=dc.doc_class_code)
-    if existing_filehash:
+    existing_class_code = get_document_class_service.get_by_docclassId(db_session=db,
+                                                                       doc_class_id=du.document_class)
+    if not existing_class_code:
         raise HTTPException(
-            status_code=400, detail="Document Class or Code already exists"
+            status_code=400, detail="Document Class does not exist!"
         )
 
-    dc.user_id = cur_user.id
-    dc.document_id = file_name_hash
+    if existing_filehash:
+        raise HTTPException(
+            status_code=400, detail="Document with same hash exists"
+        )
+
+    dc = DocumentUser_Create(
+        user_id=cur_user.id,
+        document_id=file_name_hash,
+        document_filename=file.filename,
+        document_source=du.document_source,
+        document_type=du.document_type,
+        document_class=du.document_class,
+        document_location=du.document_location,
+        document_password=du.document_password,
+        document_category_code=du.document_category_code,
+        document_state=du.document_state,
+        document_review=du.document_review,
+        document_is_deleted=du.document_is_deleted
+    )
+    # assign data from file
     return get_document_user_service.create(db_session=db, obj_in=dc)
 
 
 @router.get("/document", status_code=201, response_model=List[DocumentUser])
-async def list_doc_class(db: Session = Depends(deps.get_db),
-                         cur_user: User_Info = Depends(deps.get_current_user)
-                         ) -> List[Document_User]:
+async def list_user_documents(db: Session = Depends(deps.get_db),
+                              cur_user: User_Info = Depends(deps.get_current_user)
+                              ) -> List[Document_User]:
     return get_document_user_service.list_by_user_id(db_session=db, user_id=cur_user.id)
