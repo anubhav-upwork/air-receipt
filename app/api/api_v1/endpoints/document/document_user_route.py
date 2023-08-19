6a2c27bb9ded109f830eaa37f25ba4f969378097
anubhav <rohatgianubhav@gmail.com>
2023-08-19 20:25:39 +0530

    fixed document upload service - creates file in public/documents/<user_id> - Hashes filename to make unique filenames

--- app/api/api_v1/endpoints/document/document_user_route.py
+++ app/api/api_v1/endpoints/document/document_user_route.py
@@ -1,10 +1,11 @@
-import logging
+import os
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException, File, UploadFile, Form
 from sqlalchemy.orm import Session
-from werkzeug.security import generate_password_hash
-
+from app.core.airlogger import logger
 from app.api import deps
+from app.core import utils
+from app.core.config import settings
 from app.models.user.user_info import User_Info
 from app.models.documents.document_user import Document_User, DocumentSrc, DocumentType, DocumentState, DocumentReview
 from app.schemas.document.document_user import DocumentUser, DocumentUser_Create, DocumentUser_Update, \
@@ -13,8 +14,6 @@ from app.crud.document.document_user import get_document_user_service
 from app.crud.document.document_category import get_document_category_service
 from app.crud.document.document_class import get_document_class_service
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/documents", tags=["Document"])
 
@@ -24,11 +23,32 @@ async def upload_document(du: DocumentUser_Upload = Depends(),
                           file: UploadFile = File(...),
                           db: Session = Depends(deps.get_db),
                           cur_user: User_Info = Depends(deps.get_current_user)) -> Document_User:
+
+    _file_save_path = str(settings.UPLOAD_PATH) + "/" + str(cur_user.id)
     logger.info(f"Document Received: from:{cur_user.id}, file:{file.filename},"
                 f" State:{du.document_state},"
                 f" ClassId:{du.document_class}")
 
-    file_name_hash = generate_password_hash(file.filename, method='md5')
+    # Check if file store path exists, if not create dir
+    if not os.path.isdir(_file_save_path):
+        os.makedirs(_file_save_path)
+
+    _filename = file.filename
+    _file_content_type = file.content_type
+    _filename_len = len(file.filename.split('.'))
+
+    _extension = file.filename.split('.')[_filename_len - 1].lower()  # Save the extension of the file for upload
+    logger.info(f"File extension : <{_extension}>, filename <{_filename}>,"
+                f" uploaded by <{cur_user.user_name}>,"
+                f" content-type <{_file_content_type}>")
+
+    if _file_content_type not in settings.ALLOWED_CONTENT:
+        raise HTTPException(
+            status_code=400, detail="Content Type Not Allowed!"
+        )
+
+    file_name_hash = utils.generate_file_name(8) + f".{_extension}"
+
     existing_filehash = get_document_user_service.get_by_doc_id(db_session=db, doc_id=file_name_hash)
     existing_class_code = get_document_class_service.get_by_docclassId(db_session=db,
                                                                        doc_class_id=du.document_class)
@@ -50,10 +70,19 @@ async def upload_document(du: DocumentUser_Upload = Depends(),
             status_code=400, detail="Document with same hash exists"
         )
 
+    try:
+        content = await file.read()
+        with open(_file_save_path + "/" + file_name_hash, 'wb') as f:
+            f.write(content)
+    except Exception as ex:
+        raise HTTPException(
+            status_code=400, detail="File writing error"
+        )
+
     dc = DocumentUser_Create(
         user_id=cur_user.id,
         document_id=file_name_hash,
-        document_filename=file.filename,
+        document_filename=_filename,
         document_source=du.document_source,
         document_type=du.document_type,
         document_class=du.document_class,
---
+++ app/core/utils.py
@@ -0,0 +1,18 @@
+import magic
+import mimetypes
+import secrets
+import string
+
+
+def guessMime(file) -> str:
+    return magic.from_buffer(file, mime=True)
+
+
+def guessFileExtension(file) -> str:
+    mime = guessMime(file)
+    return mimetypes.guess_extension(mime)
+
+
+def generate_file_name(length=5) -> string:
+    filename = ''.join(secrets.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(length))
+    return filename
