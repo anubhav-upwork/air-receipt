02f8571b2b439032ba380eda401739885163f8e3
anubhav <rohatgianubhav@gmail.com>
2023-08-20 20:31:58 +0530

    Update user_auth.py -- add credit and update user details

--- app/api/api_v1/endpoints/user/user_auth.py
+++ app/api/api_v1/endpoints/user/user_auth.py
@@ -1,12 +1,15 @@
 from typing import Any
+from pydantic import condecimal
 
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm.session import Session
 
+# user info
 from app.crud.user import get_user_info_service, get_user_action_audit_service
-from app.schemas.user.user_info import UserInfo, UserInfo_Create
+from app.schemas.user.user_info import UserInfo, UserInfo_Create, UserInfo_Update
 
+# user audit
 from app.schemas.user.user_action_audit import UserAuditTrail_Create
 from app.models.user.user_action_audit import User_Action
 
@@ -58,11 +61,18 @@ def login(
 
 
 @router.get("/me", response_model=UserInfo)
-def read_users_me(current_user: UserInfo = Depends(deps.get_current_user)):
+def read_users_me(db: Session = Depends(deps.get_db), current_user: UserInfo = Depends(deps.get_current_user)):
     """
     Fetch the current logged-in user.
     """
+    # enter into user audit trail
+    audit_log = UserAuditTrail_Create(
+        user_id=current_user.id,
+        action=User_Action.update,
+        action_msg=f"User <{current_user.user_name}> accessed get user details."
+    )
 
+    user_audit = get_user_action_audit_service.create(db, audit_log)
     user = current_user
     return user
 
@@ -86,3 +96,33 @@ def create_user_signup(
     user = get_user_info_service.create(db_session=db, obj_in=user_in)
 
     return user
+
+
+@router.patch("/add_credit", status_code=201, response_model=UserInfo_Update)
+async def add_credit(credit: condecimal(decimal_places=2),
+                     db: Session = Depends(deps.get_db),
+                     current_user: UserInfo = Depends(deps.get_current_user)) -> User_Info:
+    uinfo = UserInfo_Update(
+        user_name=current_user.user_name,
+        user_mobile=current_user.user_mobile,
+        user_location=current_user.user_location,
+        user_password=current_user.user_password,
+        user_role=current_user.user_role,
+        user_type=current_user.user_type,
+        user_credit=current_user.user_credit + credit,
+        user_is_deleted=current_user.user_is_deleted,
+        user_is_active=current_user.user_is_active
+    )
+    if credit < 1.00:
+        raise HTTPException(
+            status_code=400, detail="Trying to add less than 1 credit!!"
+        )
+
+    # enter into user audit trail
+    audit_log = UserAuditTrail_Create(
+        user_id=current_user.id,
+        action=User_Action.credit_load,
+        action_msg=f"User <{current_user.user_name}> added credits {credit}."
+    )
+    user_audit = get_user_action_audit_service.create(db, audit_log)
+    return get_user_info_service.update(db_session=db, _id=current_user.id, obj=uinfo)
