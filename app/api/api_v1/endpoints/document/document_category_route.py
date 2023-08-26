9ad22e4b35b3f4430e427d2ae4e3efc29b28506f
anubhav <rohatgianubhav@gmail.com>
2023-08-26 20:24:11 +0530

    Documentation of  document_category_route.py and document_class_route.py

--- app/api/api_v1/endpoints/document/document_category_route.py
+++ app/api/api_v1/endpoints/document/document_category_route.py
@@ -1,17 +1,34 @@
 from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.models.documents.document_category import Document_Category
-from app.schemas.document.document_category import DocumentCategory, DocumentCategory_Create, DocumentCategory_Update
-from app.crud.document.document_category import get_document_category_service
+
 from app.api import deps
+from app.models.user.user_info import User_Info
+
+from app.crud.document.document_category import get_document_category_service
+from app.models.documents.document_category import Document_Category
+from app.schemas.document.document_category import DocumentCategory, DocumentCategory_Create
 
 router = APIRouter(prefix="/doc-category", tags=["Document Category"])
 
 
 @router.post("/create_doc_category", status_code=201, response_model=DocumentCategory)
 async def create_doc_category(dc: DocumentCategory_Create,
-                              db: Session = Depends(deps.get_db)) -> Document_Category:
+                              db: Session = Depends(deps.get_db),
+                              cur_user: User_Info = Depends(deps.get_current_user)) -> Document_Category:
+    """
+        API for creating Document Categories which specify to which sector it belongs to.
+    @param dc: Input Document Category Create Pydantic Object
+    @param db: Input DB session
+    @param cur_user: Input current user session to restrict access
+    @return: Output Document Category Pydantic Object
+    """
+    if not cur_user.user_is_superuser:
+        raise HTTPException(
+            status_code=401, detail="Not authorized to access this"
+        )
+
     existing_category = get_document_category_service.get_by_doccategory(db_session=db,
                                                                          doc_cat=dc.category)
     existing_cat_code = get_document_category_service.get_by_doccatcode(db_session=db,
@@ -24,5 +41,16 @@ async def create_doc_category(dc: DocumentCategory_Create,
 
 
 @router.get("/", status_code=201, response_model=List[DocumentCategory])
-async def list_doc_category(db: Session = Depends(deps.get_db)) -> List[Document_Category]:
+async def list_doc_category(db: Session = Depends(deps.get_db),
+                            cur_user: User_Info = Depends(deps.get_current_user)) -> List[Document_Category]:
+    """
+    API to list Document Categories
+    @param db: Input DB session
+    @param cur_user: Input current user session to restrict access
+    @return: Output list of Document Categories listed as list of pydantic objects
+    """
+    if not cur_user.user_is_superuser:
+        raise HTTPException(
+            status_code=401, detail="Not authorized to access this"
+        )
     return get_document_category_service.list(db_session=db)
--- app/api/api_v1/endpoints/document/document_class_route.py
+++ app/api/api_v1/endpoints/document/document_class_route.py
@@ -1,17 +1,38 @@
 from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.models.documents.document_class import Document_Class, Doc_Class
-from app.schemas.document.document_class import DocumentClass, DocumentClass_Create, DocumentClass_Update
-from app.crud.document.document_class import get_document_class_service
+
 from app.api import deps
+from app.models.user.user_info import User_Info
+from app.crud.document.document_class import get_document_class_service
+from app.models.documents.document_class import Document_Class
+from app.schemas.document.document_class import DocumentClass, DocumentClass_Create
 
 router = APIRouter(prefix="/doc-class", tags=["Document Class"])
 
 
 @router.post("/create_doc_class", status_code=201, response_model=DocumentClass)
 async def create_doc_class(dc: DocumentClass_Create,
-                           db: Session = Depends(deps.get_db)) -> Document_Class:
+                           db: Session = Depends(deps.get_db),
+                           cur_user: User_Info = Depends(deps.get_current_user)) -> Document_Class:
+    """
+        API for creating Document Class. Document Classes can be either of these below:
+            INVOICE	1000
+            RECEIPT	1001
+            CREDIT_NOT	1002
+            OTHER	1003
+    @param cur_user: Input Current User (Dependency Injection)
+    @param dc: Input Document Class Create Pydantic Object
+    @param db: Input DB session
+    @return: Outputs Document Class Pydantic
+    """
+
+    if not cur_user.user_is_superuser:
+        raise HTTPException(
+            status_code=401, detail="Not authorized to access this"
+        )
+
     existing_class = get_document_class_service.get_by_docclass(db_session=db, doc_class=dc.doc_class)
     existing_class_code = get_document_class_service.get_by_docclassCode(db_session=db,
                                                                          doc_class_code=dc.doc_class_code)
@@ -23,5 +44,18 @@ async def create_doc_class(dc: DocumentClass_Create,
 
 
 @router.get("/", status_code=201, response_model=List[DocumentClass])
-async def list_doc_class(db: Session = Depends(deps.get_db)) -> List[Document_Class]:
+async def list_doc_class(db: Session = Depends(deps.get_db),
+                         cur_user: User_Info = Depends(deps.get_current_user)) -> List[Document_Class]:
+    """
+    API for listing all the stored Document Classes
+    @param cur_user: Input Current User (Dependency Injection)
+    @param db: Input DB session
+    @return: Outputs List Document Class Pydantic
+    """
+
+    if not cur_user.user_is_superuser:
+        raise HTTPException(
+            status_code=401, detail="Not authorized to access this"
+        )
+
     return get_document_class_service.list(db_session=db)
