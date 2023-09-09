f71a5abff2fdfd02fcaa340c1f0e525982480d75
anubhav <rohatgianubhav@gmail.com>
2023-09-09 14:17:11 +0530

    Update kafka_document_schema.py and kafka_todo_schema.py

---
+++ app/schemas/document/kafka_document_schema.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+from app.schemas.schema_utils import to_camel
+
+
+class Kafka_Document(BaseModel):
+    user_id: int
+    document_id: str
+    document_filename: str
+    document_pages: int
+    document_size: float = 0.0
+    document_push_time: str = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+    class Config:
+        from_attributes = True
+        alias_generator = to_camel
+        populate_by_name = True
