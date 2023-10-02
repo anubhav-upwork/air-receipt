98de3e547cfd3df931584350d9600b3696055ced
anubhav <rohatgianubhav@gmail.com>
2023-10-02 19:47:50 +0530

    Update __init__.py

--- app/crud/document/__init__.py
+++ app/crud/document/__init__.py
@@ -6,3 +6,5 @@ __author__ = 'Anubhav Rohatgi'
 
 from .document_class import get_document_class_service
 from .document_category import get_document_category_service
+from .document_audit import get_document_audit_service
+from .document_user import get_document_user_service
