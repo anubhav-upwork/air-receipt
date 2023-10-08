9398d101488737a2f8961d7ae133c4f953b5a78f
anubhav <rohatgianubhav@gmail.com>
2023-10-08 16:24:27 +0530

    documentation and upped version __init__.py, config.py, and 3 more files...

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,4 @@
   Created by Anubhav Rohatgi on 24/07/2023.
 """
 __author__ = 'Anubhav Rohatgi'
+__version__ = '0.4.0'
--- app/core/config.py
+++ app/core/config.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 days
 
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
-    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
+    # e.g.: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
