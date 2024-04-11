a3b48bc5fa37d9eb36dd3e05169241bd701a3b08
anubhavrohatgi <rohatgianubhav@gmail.com>
2024-04-11 19:20:23 +0530

    fake integration.py updated

--- app/api/api_v1/endpoints/integrations/dummy_integration.py
+++ app/api/api_v1/endpoints/integrations/dummy_integration.py
@@ -1,9 +1,11 @@
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException, status
+
+from fastapi import APIRouter, Depends, status
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from typing import Optional
-from pydantic import BaseModel, Field
+
 from app.api import deps
+from app.core.http_exceptions import NotFoundException
 
 router = APIRouter(prefix="/integrations", tags=["Integrations"])
 
@@ -11,15 +13,16 @@ router = APIRouter(prefix="/integrations", tags=["Integrations"])
 class Integrations(BaseModel):
     id: int
     integration_app: str
+    is_active: bool
 
     class Config:
         populate_by_name = True
 
 
 fake_db_integrations: List[Integrations] = [
-    Integrations(id=0, integration_app="Xerox"),
-    Integrations(id=1, integration_app="Dext"),
-    Integrations(id=2, integration_app="Finance")
+    Integrations(id=0, integration_app="Xerox", is_active=False),
+    Integrations(id=1, integration_app="Dext", is_active=False),
+    Integrations(id=2, integration_app="Finance", is_active=False)
 ]
 
 
@@ -29,10 +32,20 @@ async def list_integrations(db: Session = Depends(deps.get_db)) -> List[Integrat
 
 
 @router.get("/activate_integration", status_code=status.HTTP_200_OK)
-async def activate_integration(_id: int, name: str, db: Session = Depends(deps.get_db)) -> bool:
-    check_this = Integrations(id=_id, integration_app=name),
+async def activate_integration(_id: int, name: str, db: Session = Depends(deps.get_db)) -> Integrations:
+    try:
+        check_this = next(x for x in fake_db_integrations if (x.integration_app == name and x.id == _id))
+        check_this.is_active = True
+    except Exception as ex:
+        raise NotFoundException(f"Integration {name} not found in db, check type case and spelling")
+    return check_this
 
-    if check_this in fake_db_integrations:
-        return True
-    else:
-        return False
+
+@router.get("/deactivate_integration", status_code=status.HTTP_200_OK)
+async def deactivate_integration(_id: int, name: str, db: Session = Depends(deps.get_db)) -> Integrations:
+    try:
+        check_this = next(x for x in fake_db_integrations if (x.integration_app == name and x.id == _id))
+        check_this.is_active = True
+    except Exception as ex:
+        raise NotFoundException(f"Integration {name} not found in db, check type case and spelling")
+    return check_this
